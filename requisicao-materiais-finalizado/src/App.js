c0ee6b488f557e2faa3f62f824f7fc6619d92ce3
simdigitalrequest <simdigital62@gmail.com>
2025-07-25 11:31:01 -0300

    Add files via upload

---
+++ requisicao-materiais-finalizado/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sistema de Requisição de Materiais</h1>
+      <p>Em desenvolvimento...</p>
+    </div>
+  );
+}
+
+export default App;
